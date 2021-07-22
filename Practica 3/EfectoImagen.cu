5bff865c11b7bfcb373f4f2d6be5ec057d2056c1
Edisson <edprietov@unal.edu.co>
2021-07-21 21:32:55 -0500

    Inicio práctica 3

---
+++ Practica 3/EfectoImagen.cu
@@ -0,0 +1,84 @@
+//Programa que implementa el algoritmo de aplicación de algunos filtros sobre una imagen
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <ctime>
+#include <fstream>
+
+using namespace cv;
+using namespace std;
+
+
+__global__
+void saxpy()
+{
+
+}
+
+//Prototipos de funciones
+Mat lectura_imagen(String nombre_imagen);
+void filtro_amarillo(String nombre);
+ofstream archivo;
+
+
+int main(int argc, char **argv)
+{
+    filtro_amarillo(argv[1]);
+    waitKey(0);
+    return 0;
+}
+
+//***filtro2*****
+void filtro_amarillo(String nombre)
+{
+    int pixel = 0;
+    unsigned tiempo_final, tiempo_inicial;
+    Mat imagen ;
+
+        imagen = lectura_imagen(nombre);
+        tiempo_inicial = clock();
+
+        for (int y = 0; y < imagen.rows; y++)
+        {
+            for (int x = 0; x < imagen.cols; x++)
+            {
+                Vec3b color = imagen.at<Vec3b>(y, x);
+                int azul = (int)color.val[0];
+                int verde = (int)color.val[1];
+                int rojo = (int)color.val[2];
+                int promedio = (int)((azul + verde + rojo) / 3);
+
+                if( rojo > 200 && verde > 100 && azul < 85  ){
+                color.val[0] = azul;
+                color.val[1] = verde;
+                color.val[2] = rojo;
+                }
+                else{
+                color.val[0] = 255;
+                color.val[1] = 255;
+                color.val[2] = 255;
+                }
+                
+                imagen.at<Vec3b>(y, x) = color;
+            }
+        }
+    imwrite("filtradacolor" + nombre, imagen);
+}
+
+//*****Procedimiento que lee la imagen******
+Mat lectura_imagen(String nombre_imagen)
+{
+    // Lectura de la imagen
+    Mat imagen = imread(nombre_imagen, 1);
+
+    // Manejo de error en caso de que no sea encontrada la imagen
+    if (imagen.empty())
+    {
+        cout << "Archivo de imagen "
+             << "No encontrado" << endl;
+        cin.get();
+        return imagen;
+    }
+    return imagen;
+}
---
+++ Practica 3/README.md
@@ -0,0 +1,15 @@
+# Práctica 3
+
+Aplicación de filtro sobre una imagen de forma secuencial y usando CUDA.
+
+Se deben ejecutar los siguientes comandos:
+
+> cmake .
+> 
+> make
+> 
+> ./EfectoImagen "Nombre de la imagen a la que se le quiere aplicar el filtro" 
+>
+> 
+
+####  Nota: Se debe tener instalada la librería OpenCV para C++,  ademas CUDA.
